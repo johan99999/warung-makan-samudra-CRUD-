7a8a01d77bcfd13374d5a0a6ae409c6a9a1780fa
hanlie <johansama9999@gmail.com>
2024-08-15 11:01:07 +0700

    [START] Menambahkan Project Management Maven Kedalam Directory Project

---
+++ src/main/java/warungmakansamudracrud/crud/WarungMakanSamudraCrudApplication.java
@@ -0,0 +1,13 @@
+package warungmakansamudracrud.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarungMakanSamudraCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarungMakanSamudraCrudApplication.class, args);
+	}
+
+}
